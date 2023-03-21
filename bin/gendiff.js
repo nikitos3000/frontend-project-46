7014cc91afc5da2b97e792fc2f466b15d3785a36
nikita <trinevnikita@gmail.com>
2023-03-21 12:28:55 +0300

    stage2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,10 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .option('-V, --version        output the version number')
+  .helpOption('-h, --help           output usage information')
+  .option('-f, --format <type>  output format')
 program.parse();
