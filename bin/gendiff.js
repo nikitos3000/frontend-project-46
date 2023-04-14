0b7ce5c2e6f0383b694fc97a9a52bd91675de8ef
nikita <trinevnikita@gmail.com>
2023-04-14 14:10:59 +0300

    fixfinalnogofixa

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from 'commander';
 import showDiff from '../src/index.js';
 
@@ -6,8 +7,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version      output the version number')
+  .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format', 'stylish')
+  .helpOption('-h, --help', 'output usage information')
   .argument('<filepath1>')
   .argument('<filepath2>')
   .action((filepath1, filepath2) => {
